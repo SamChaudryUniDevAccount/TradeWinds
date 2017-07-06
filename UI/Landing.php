1c3717df543462a8777dfb12af369f6afc102b42
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-06 16:11:39 +0100

    script tag changes

--- UI/Landing.php
+++ UI/Landing.php
@@ -9,12 +9,20 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- CSS-->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-grid.css">
+    <link rel="stylesheet" href="css/styles.css">
     <meta charset="UTF-8">
-    <title>Trade Winds</title>
+    <title>Trade Winds Landing</title>
+
 </head>
 
 <body>
         Welcome to Trade Winds
 </body>
-
+<!-- JavaScript-->
+<script src="javascript/jquery.js"></script>
+<script src="javascript/login.js"></script>
+<script src="javascript/bootstrap.js"></script>
 </html>

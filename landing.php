d2562bd1636890d80bf2b4f0d9f4ef59f7dda6e6
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-07 13:57:34 +0100

    Addition of cards

--- landing.php
+++ landing.php
@@ -20,56 +20,53 @@ session_start();
 
 </head>
 <body>
+<!-- Navbar start-->
+        <nav id="navBar" class="navbar navbar-toggleable-md navbar-light bg-faded">
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <a id="navBarBrand" class="navbar-brand" href="#">Trade Winds </a>
+            <div class="collapse navbar-collapse" id="main_navbar">
+                <ul class="nav navbar-nav mr-auto"></ul>
+                <ul class="nav navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Settings</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Log out</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <!-- End of navbar-->
        <div class="container-fluid">
-           <!-- Navbar start-->
-           <nav id="navBar" class="navbar navbar-toggleable-md navbar-light bg-faded">
-               <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                   <span class="navbar-toggler-icon"></span>
-               </button>
-               <a id="navBarBrand" class="navbar-brand" href="#">Trade Winds </a>
-                   <div class="collapse navbar-collapse" id="main_navbar">
-                       <ul class="nav navbar-nav mr-auto"></ul>
-                       <ul class="nav navbar-nav">
-                           <li class="nav-item">
-                               <a class="nav-link" href="#">Settings</a>
-                           </li>
-                           <li class="nav-item">
-                               <a class="nav-link" href="#">Log out</a>
-                           </li>
-                       </ul>
-                   </div>
-           </nav>
-           <!-- End of navbar-->
-       </div>
+           <div class="pannelSection">
+               <!-- Card Deck-->
+               <div class="card-deck-wrapper">
+                   <div class="card-deck">
 
-       <div class="pannelSection">
-           <!-- Card Deck-->
-           <div class="card-deck-wrapper">
-               <div class="card-deck">
+                       <!-- Card 1 -->
+                       <div class="card">
+                           <div class="card-header">Sun Gone</div>
+                           <div class="card-block">
+                               <p class="card-text">The top resources for all things related to the Sun.</p>
+                               <a href="#" class="card-link">Sun Gone</a>
+                               <a href="#" class="card-link">Still Gone</a>
+                           </div>
+                       </div>
 
-                   <!-- Card 1 -->
-                   <div class="card">
-                       <div class="card-header">Sun Gone</div>
-                       <div class="card-block">
-                           <p class="card-text">The top resources for all things related to the Sun.</p>
-                           <a href="#" class="card-link">Sun Gone</a>
-                           <a href="#" class="card-link">Still Gone</a>
+                       <!-- Card 2 -->
+                       <div class="card">
+                           <div class="card-header">Sun Up</div>
+                           <div class="card-block">
+                               <p class="card-text">Looks like the Sun has returned. Here's <a href="#" class="card-link">why</a>.</p>
+                           </div>
                        </div>
                    </div>
-
-                   <!-- Card 2 -->
-                   <div class="card">
-                       <div class="card-header">Sun Up</div>
-                       <div class="card-block">
-                           <p class="card-text">Looks like the Sun has returned. Here's <a href="#" class="card-link">why</a>.</p>
-                       </div>
-                   </div>
-
                </div>
            </div>
+
        </div>
-
-
 
 </body>
 <!-- JavaScript-->

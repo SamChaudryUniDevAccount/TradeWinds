a7f6154e243ab806f2da76dcf0df103ea7af1c9b
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-07 11:19:08 +0100

    Landing Page UI +  new js file

--- index.php
+++ index.php
@@ -47,7 +47,9 @@ else{
     <!DOCTYPE html>
     <html>
     <head>
-        <meta charset="UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Trade Winds Login </title>
         <!-- CSS-->
         <link rel="stylesheet" href="css/bootstrap.css">
--- landing.php
+++ landing.php
@@ -9,59 +9,49 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- CSS-->
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/bootstrap-grid.css">
     <link rel="stylesheet" href="css/styles.css">
-    <meta charset="UTF-8">
     <title>Trade Winds Landing</title>
 
 </head>
 <body>
        <div class="container-fluid">
-           <nav class = "navbar navbar-default" role = "navigation">
-
-               <div class = "navbar-header">
-                   <button type = "button" class = "navbar-toggle"
-                           data-toggle = "collapse" data-target = "#example-navbar-collapse">
-                       <span class = "sr-only">Toggle navigation</span>
-                       <span class = "icon-bar"></span>
-                       <span class = "icon-bar"></span>
-                       <span class = "icon-bar"></span>
+           <!-- Navbar start-->
+           <nav class="navbar navbar-default">
+               <!-- Brand and toggle get grouped for better mobile display -->
+               <div class="navbar-header">
+                   <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                       <span class="sr-only">Toggle navigation</span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
                    </button>
-
-                   <a class = "navbar-brand" href = "#">TutorialsPoint</a>
+                   <a href="#" class="navbar-brand">Brand</a>
                </div>
-
-               <div class = "collapse navbar-collapse" id = "example-navbar-collapse">
-
-                   <ul class = "nav navbar-nav">
-                       <li class = "active"><a href = "#">iOS</a></li>
-                       <li><a href = "#">SVN</a></li>
-
-                       <li class = "dropdown">
-                           <a href = "#" class = "dropdown-toggle" data-toggle = "dropdown">
-                               Java
-                               <b class = "caret"></b>
-                           </a>
-
-                           <ul class = "dropdown-menu">
-                               <li><a href = "#">jmeter</a></li>
-                               <li><a href = "#">EJB</a></li>
-                               <li><a href = "#">Jasper Report</a></li>
-
-                               <li class = "divider"></li>
-                               <li><a href = "#">Separated link</a></li>
-
-                               <li class = "divider"></li>
-                               <li><a href = "#">One more separated link</a></li>
-                           </ul>
-
-                       </li>
-
+               <!-- Trade Winds options -->
+               <div id="navbarCollapse" class="collapse navbar-collapse">
+                   <ul class="nav navbar-nav">
+                       <li class="active"><a href="#">Home</a></li>
+                       <li><a href="#">Profile</a></li>
+                       <li><a href="#">Messages</a></li>
+                   </ul>
+                   <ul class="nav navbar-nav navbar-right">
+                       <li><a href="#">Login</a></li>
                    </ul>
                </div>
            </nav>
+           <!-- End-->
+
+
+
+
+
+
 
        </div>
 </body>

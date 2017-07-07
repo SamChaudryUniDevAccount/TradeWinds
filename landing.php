9401c21d620eb82d45f09a5f8bee868f38cbe358
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-07 11:57:41 +0100

    navbar fixes

--- landing.php
+++ landing.php
@@ -23,10 +23,25 @@ session_start();
        <div class="container-fluid">
            <!-- Navbar start-->
            <nav id="navBar" class="navbar navbar-toggleable-md navbar-light bg-faded">
-               <a class="navbar-brand mr-0 ml-lg-auto hidden-xs-down" href="#" target="_blank">Trade Winds</a>
-               <div class="nav navbar-nav">
-                   <a class="nav-item nav-link" href="#">Settings <span class="sr-only">(current)</span></a>
-                   <a class="nav-item nav-link" href="#">About</a>
+               <div class="navbar-header">
+                   <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                       <span class="sr-only">Toggle navigation</span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                   </button>
+                   <a href="#" class="navbar-brand">Brand</a>
+               </div>
+               <!-- Collection of nav links and other content for toggling -->
+               <div id="navbarCollapse" class="collapse navbar-collapse">
+                   <ul class="nav navbar-nav">
+                       <li class="active"><a href="#">Home</a></li>
+                       <li><a href="#">Profile</a></li>
+                       <li><a href="#">Messages</a></li>
+                   </ul>
+                   <ul class="nav navbar-nav navbar-right">
+                       <li><a href="#">Login</a></li>
+                   </ul>
                </div>
            </nav>
            <!-- End-->

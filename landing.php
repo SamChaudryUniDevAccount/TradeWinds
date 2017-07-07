424513246f77819b10c79e205259a6f23c15b27c
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-07 11:22:55 +0100

    Landing Page UI +  new js file

--- landing.php
+++ landing.php
@@ -23,16 +23,6 @@ session_start();
        <div class="container-fluid">
            <!-- Navbar start-->
            <nav id="navBar" class="navbar navbar-default">
-               <!-- Brand and toggle get grouped for better mobile display -->
-               <div class="navbar-header">
-                   <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-                       <span class="sr-only">Toggle navigation</span>
-                       <span class="icon-bar"></span>
-                       <span class="icon-bar"></span>
-                       <span class="icon-bar"></span>
-                   </button>
-                   <a href="#" class="navbar-brand">Brand</a>
-               </div>
                <!-- Trade Winds options -->
                <div id="navbarCollapse" class="collapse navbar-collapse">
                    <ul class="nav navbar-nav">

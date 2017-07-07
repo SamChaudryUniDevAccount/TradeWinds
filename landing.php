7b9101384ebb503dfdc036fc0f8b2b4332672f38
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-07 13:43:45 +0100

    Addition of cards

--- landing.php
+++ landing.php
@@ -40,36 +40,32 @@ session_start();
                    </div>
            </nav>
            <!-- End of navbar-->
-           <!-- End of main page container-->
-
-
-           <div class="card-deck-wrapper">
-               <div class="card-deck">
-
-                   <!-- Card 1 -->
-                   <div class="card">
-                       <div class="card-header">Sun Gone</div>
-                       <div class="card-block">
-                           <p class="card-text">The top resources for all things related to the Sun.</p>
-                           <a href="#" class="card-link">Sun Gone</a>
-                           <a href="#" class="card-link">Still Gone</a>
-                       </div>
-                   </div>
-
-                   <!-- Card 2 -->
-                   <div class="card">
-                       <div class="card-header">Sun Up</div>
-                       <div class="card-block">
-                           <p class="card-text">Looks like the Sun has returned. Here's <a href="#" class="card-link">why</a>.</p>
-                       </div>
-                   </div>
-
-               </div>
-           </div>
-
-
-
        </div>
+
+       <div class="card-deck-wrapper">
+           <div class="card-deck">
+
+               <!-- Card 1 -->
+               <div class="card">
+                   <div class="card-header">Sun Gone</div>
+                   <div class="card-block">
+                       <p class="card-text">The top resources for all things related to the Sun.</p>
+                       <a href="#" class="card-link">Sun Gone</a>
+                       <a href="#" class="card-link">Still Gone</a>
+                   </div>
+               </div>
+
+               <!-- Card 2 -->
+               <div class="card">
+                   <div class="card-header">Sun Up</div>
+                   <div class="card-block">
+                       <p class="card-text">Looks like the Sun has returned. Here's <a href="#" class="card-link">why</a>.</p>
+                   </div>
+               </div>
+
+           </div>
+       </div>
+
 </body>
 <!-- JavaScript-->
 <script src="javascript/jquery.js"></script>

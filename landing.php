3dfaedb1a24a8390563dafa837ae94742fb9a79c
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-07 12:03:42 +0100

    navbar fixes

--- landing.php
+++ landing.php
@@ -22,7 +22,7 @@ session_start();
 <body>
        <div class="container-fluid">
            <!-- Navbar start-->
-           <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+           <nav id="navBar" class="navbar navbar-toggleable-md navbar-light bg-faded">
                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                    <span class="navbar-toggler-icon"></span>
                </button>

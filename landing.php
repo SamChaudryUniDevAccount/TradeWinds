3cac31376f37b8ea4ea04f885a33ddc2df011d36
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-07 11:59:07 +0100

    navbar fixes

--- landing.php
+++ landing.php
@@ -24,12 +24,6 @@ session_start();
            <!-- Navbar start-->
            <nav id="navBar" class="navbar navbar-toggleable-md navbar-light bg-faded">
                <div class="navbar-header">
-                   <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
-                       <span class="sr-only">Toggle navigation</span>
-                       <span class="icon-bar"></span>
-                       <span class="icon-bar"></span>
-                       <span class="icon-bar"></span>
-                   </button>
                    <a href="#" class="navbar-brand">Brand</a>
                </div>
                <!-- Collection of nav links and other content for toggling -->

893c36ce08c549284837a425a4ac9085d9841bb2
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-07 11:34:02 +0100

    Landing Page UI nav

--- landing.php
+++ landing.php
@@ -28,16 +28,14 @@ session_start();
                </button>
                <a class="navbar-brand" href="#">Navbar</a>
                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                   <ul class=" .float-xs-right navbar-nav">
+                   <ul class=" .float-lg-right navbar-nav">
                        <li class="nav-item active">
                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
                        </li>
                        <li class="nav-item">
                            <a class="nav-link" href="#">Link</a>
                        </li>
-                       <li class="nav-item">
-                           <a class="nav-link disabled" href="#">Disabled</a>
-                       </li>
+
                    </ul>
                </div>
            </nav>

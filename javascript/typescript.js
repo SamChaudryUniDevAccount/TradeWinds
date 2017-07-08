6024c3383fe68263289c5744082c5510490f1ab8
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-08 12:09:45 +0100

    tyoescript test

--- javascript/typescript.js
+++ javascript/typescript.js
@@ -1,2 +1,17 @@
+$(document).ready(function () {
+    alert("This came from typescript.js document.ready function");
+    getSuccessOutput();
+});
 var user = "sam this came from sam who is using typescript";
 document.getElementById('test').innerHTML = user;
+function getSuccessOutput() {
+    $.ajax({
+        url: 'http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?',
+        complete: function (response) {
+            alert(response);
+        },
+        error: function () {
+        },
+    });
+    return false;
+}

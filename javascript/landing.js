26591417e52f1e12b5c68a76a6e5c293b3e22b65
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-08 10:30:22 +0100

    dependencies update

--- javascript/landing.js
+++ javascript/landing.js
@@ -7,31 +7,42 @@ $( document ).ready(function() {
 
 function loadGraph() {
 
-    Highcharts.chart('commoditiesGraph', {
+        //Sample from test project integrated in for hard coded data
+    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data)    {
 
-        chart: {
-            renderTo: 'commoditiesGraph'
-        },
+        var CommoditiesDataObject = {
 
-        xAxis: {
-            ordinal: false
-        },
+            rangeSelector: {
+                selected: 1,
 
-        rangeSelector: {
-            selected: 1
-        },
+            },
 
-        plotOptions: {
-            line: {
-                gapSize: 2
+            title: {
+                text: 'AAPL Stock Price'
+            },
+
+            series: [{
+                name: 'AAPL',
+                data: data,
+                tooltip: {
+                    valueDecimals: 2
+                }
+            }],
+
+            chart: {
+                renderTo: 'container'
             }
-        },
+        };
 
-        series: [{
-            name: 'USD to EUR',
-            data: usdeur
-        }]
+        //var chart = new Highcharts.StockChart(dataObject);
+
+        var plot = $('#container').highcharts('commoditiesGraph', dataObject);
     });
+
+
+
+
+
 
 //Graph commodities id is: commoditiesGraph
 }

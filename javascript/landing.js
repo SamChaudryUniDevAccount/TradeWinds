d1c7e6793be334bf8c2b059eacef5b9a10ac96e1
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-08 10:27:11 +0100

    dependencies update

--- javascript/landing.js
+++ javascript/landing.js
@@ -9,55 +9,29 @@ function loadGraph() {
 
     Highcharts.chart('commoditiesGraph', {
 
-        title: {
-            text: 'Solar Employment Growth by Sector, 2010-2016'
+        chart: {
+            renderTo: 'commoditiesGraph'
         },
 
-        subtitle: {
-            text: 'Source: thesolarfoundation.com'
+        xAxis: {
+            ordinal: false
         },
 
-        yAxis: {
-            title: {
-                text: 'Number of Employees'
-            }
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
+        rangeSelector: {
+            selected: 1
         },
 
         plotOptions: {
-            series: {
-                pointStart: 2010
+            line: {
+                gapSize: 2
             }
         },
 
         series: [{
-            name: 'Installation',
-            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
-        }, {
-            name: 'Manufacturing',
-            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
-        }, {
-            name: 'Sales & Distribution',
-            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387]
-        }, {
-            name: 'Project Development',
-            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227]
-        }, {
-            name: 'Other',
-            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111]
+            name: 'USD to EUR',
+            data: usdeur
         }]
-
     });
-
-
-
-
-
-
 
 //Graph commodities id is: commoditiesGraph
 }

115b05c814af0aeb6f81e57d2306940eb4995242
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-10 12:04:00 +0100

    services call

--- API/api.php
+++ API/api.php
@@ -2,9 +2,6 @@
 
 include("/config/dbconfig.php");
 
-//baseURL for Quandl
-
-//Dark skies for weather
 
 if(isset($_GET['getAssetClassList'])){
 
@@ -13,36 +10,12 @@ if(isset($_GET['getAssetClassList'])){
 
 function getAssetClassList(){
 
-    $queryError = 204;
-
     global $link;
 
     $sql = "SELECT * FROM user";
 
-
-    //$sql = " SELECT * FROM users.user";
-
     $result = mysqli_query($link,$sql);
 
     echo "Result from user table  is...".var_dump($result);
-
-    $jsonData = array();
-
-    while($row = mysqli_fetch_assoc($result)) {
-
-        $jsonData[] = $row;
-
-    }
-
-    if(count($jsonData) > 0){
-
-        echo json_encode($jsonData);
-
-    }else{
-
-
-        echo $queryError;
-    }
-
 
 }

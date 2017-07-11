182834807723ebee09a7bcbd9c19a75d61b07ae3
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-11 09:58:46 +0100

    event handler

--- apipost.php
+++ apipost.php
@@ -4,52 +4,8 @@ include("config/dbconfig.php");
 
 if (isset($_POST["getCommodityByAssetClass"])) {
 
-    global $link;
-    $commodityType= "";
+echo "Hi...";
 
-    $data = json_decode($_POST["getCommodityByAssetClass"],true);
-
-
-    foreach ($data as $key=>$value){
-
-        if($key == "commodityAssetClass"){
-
-            $commodityType = $value;
-
-        }else{
-
-            echo "Sorry asset Class unknown";
-
-        }
-
-    }
-
-    $sql = "SELECT Commodity_name FROM commodities_markets.".$commodityType;
-
-    echo $sql;
-
-    if(mysqli_query($link, $sql)){
-
-        $result = mysqli_query($link,$sql);
-
-        echo $result;
-
-        $jsonData = array();
-
-        while($row = mysqli_fetch_assoc($result)) {
-
-            $jsonData[] = $row;
-
-        }
-
-        echo json_encode($jsonData);
-
-
-    } else{
-
-        echo "Error";
-
-    }
 
 }
 

92869c94a8a3e7d1bad8b39a0b919e71b48a2421
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-12 14:23:39 +0100

    commodities landing page data

--- apipost.php
+++ apipost.php
@@ -25,7 +25,7 @@ function getCommodityData(){
      //APIkey = kv_y-Xcvsk1h3wQ1TNPE
     $baseUrl = "https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=kv_y-Xcvsk1h3wQ1TNPE";
 
-    echo json_decode($baseUrl["dataset_data"]);
+    echo json_decode($baseUrl);
 
 
 }

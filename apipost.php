e6c18829fc234a8b0eed1f5dd73171802710abab
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-13 22:59:15 +0100

    alert removal

--- apipost.php
+++ apipost.php
@@ -68,13 +68,13 @@ function getCommodityData(){
 
     $commidityUrl = "https://www.quandl.com/api/v3/datasets/".$commodityEndPoint."/"."data.json"."?"."start_date=".$start_date."&"."end_date=".$end_date."kv_y-Xcvsk1h3wQ1TNPE";
 
-    //echo $commidityUrl;
+    echo $commidityUrl;
 
     $commoditydata = file_get_contents($commidityUrl);
 
     $dateToReturn = json_decode($commoditydata,true);
 
-    echo json_encode($dateToReturn);
+   // echo json_encode($dateToReturn);
 
 }
 

b02d1a0db512bf182d87250b02ee203bed6a4acc
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-13 14:39:02 +0100

    sql string changes

--- apipost.php
+++ apipost.php
@@ -68,9 +68,9 @@ function getCommodityData(){
     //Working correctly building up the url
     $commidityUrl = "https://www.quandl.com/api/v3/datasets/".$commodityEndPoint."/"."data.json"."?"."start_date=".$start_date."&"."end_date=".$end_date."&api_key=kv_y-Xcvsk1h3wQ1TNPE";
 
-    $commoditydata =file_get_contents($commidityUrl);
+    $commoditydata = file_get_contents($commidityUrl);
 
-    echo  json_decode($commoditydata);
+    echo  json_decode($commoditydata,true);
 
 }
 

bd7f10b760dc3bcb8b7fb2bdad1426235c6b7d3d
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-07-25 14:35:09 +0100

    if else statement removal

--- apipost.php
+++ apipost.php
@@ -127,7 +127,8 @@ function getCommodityData(){
      //429 to many requests
     //echo var_dump($http_response_header, $dateToReturn);
 
-    echo json_encode($dateToReturn);
+    echo $commidityUrl;
+    //echo json_encode($dateToReturn);
 
 }
 

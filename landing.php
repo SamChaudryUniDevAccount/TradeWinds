f5421b2fc0b3f5d8999f4c3d5e429ca072b91e6d
SamChaudryUniDevAccount <s.s.chaudry@rgu.ac.uk>
2017-08-26 12:07:19 +0100

    cosmetic changes

--- landing.php
+++ landing.php
@@ -21,7 +21,6 @@ session_start();
     <title>Trade Winds Landing</title>
 </head>
 <body>
-<!-- Navbar start-->
         <nav id="navBar" class="navbar navbar-toggleable-md navbar-light bg-faded">
             <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
